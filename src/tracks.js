e8c7483cbddc822368ae933ff842a5fe5b74d6a9
Elena Strakhova <elenastrakhova@gmail.com>
2022-12-12 19:33:23 -0500

    first commit

---
+++ src/tracks.js
@@ -0,0 +1,100 @@
+import song1 from "./assets/Bohemian Rhapsody.mp3";
+import song2 from "./assets/Send in the Clowns.mp3";
+import song3 from './assets/Im Not in Love.mp3';
+import song4 from './assets/18 with a bullet.mp3';
+import song5 from './assets/D-I-V-O-R-C-E.mp3';
+import song6 from './assets/disco queen.mp3';
+
+
+export const tracks = [
+  {
+    audioSrc: song1,
+    title: "Bohemian Rhapsody",
+    artist: "Queen"
+  },
+  {
+    audioSrc: song2,
+    title: "Send in the Clowns",
+    artist: "David Davidson & Christopher Phillips"
+  },
+  {
+    audioSrc: song3,
+    title: `I'm Not In Love`,
+    artist: "10CC"
+  },
+  {
+    audioSrc: song4,
+    title: "Eighteen With a Bullet",
+    artist: "Pete Wingfield"
+  },
+  {
+    audioSrc: song5,
+    title: "D.I.V.O.R.C.E.",
+    artist: "Tammy Wynette"
+  },
+  {
+    audioSrc: song6,
+    title: "Disco Queen",
+    artist: "Hot Chocolate"
+  },
+  // {
+  //   audioSrc: song7,
+  //   title: "Stand By Your Man",
+  //   artist: "Tammy Wynette"
+  // },
+  // {
+  //   audioSrc: song8,
+  //   title: "Sailing",
+  //   artist: "Rod Stewart"
+  // },
+  // {
+  //   audioSrc: song9,
+  //   title: "Entertainment",
+  //   artist: "Phoenix"
+  // },
+  // {
+  //   audioSrc: song10,
+  //   title: "Entertainment",
+  //   artist: "Phoenix"
+  // },
+  // {
+  //   audioSrc: song11,
+  //   title: "Entertainment",
+  //   artist: "Phoenix"
+  // },
+  // {
+  //   audioSrc: song12,
+  //   title: "Entertainment",
+  //   artist: "Phoenix"
+  // },
+  // {
+  //   audioSrc: song13,
+  //   title: "Entertainment",
+  //   artist: "Phoenix"
+  // },
+  // {
+  //   audioSrc: song14,
+  //   title: "Entertainment",
+  //   artist: "Phoenix"
+  // },
+  // {
+  //   audioSrc: song15,
+  //   title: "Entertainment",
+  //   artist: "Phoenix"
+  // },
+  // {
+  //   audioSrc: song16,
+  //   title: "Entertainment",
+  //   artist: "Phoenix"
+  // },
+  // {
+  //   audioSrc: song17,
+  //   title: "Entertainment",
+  //   artist: "Phoenix"
+  // },
+  // {
+  //   audioSrc: song18,
+  //   title: "Entertainment",
+  //   artist: "Phoenix"
+  // },
+];
