fd056a405c88d2c21ef77184dba833e34abef66f
Elena Strakhova <elenastrakhova@gmail.com>
2023-01-03 18:04:16 -0500

    added songs and design

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import { tracks } from  './tracks';
 
 function App() {
   return (
-      <div className="container my-5 rounded-3">
+      <div className="my-5 rounded-3">
         <div className="row p-4 pe-lg-0 pt-lg-5 align-items-center">
           <div className="col-lg-7 p-3 p-lg-5 pt-lg-3">
-            <h1 className="display-4 fw-bold lh-1">Welcome to the magical Pog Hill and Lansquenet-sous-Tannes!</h1>
+            <h1 className="display-4 fw-bold lh-1">Blackberry Wine by Joanne Harris</h1>
             <br></br>
-            <p>As a boy, writer Jay Mackintosh spent three golden summers in the ramshackle home of "Jackapple Joe" Cox. A lonely child, he found solace in Old Joe's simple wisdom and folk charms. The magic was lost, however, when Joe disappeared without warning one fall. Years later, Jay's life is stalled with regret and ennui. His bestselling novel, Jackapple Joe, was published ten years earlier and he has written nothing since. Impulsively, he decides to leave his urban life in London and, sight unseen, purchases a farmhouse in the remote French village of Lansquenet. There, in that strange and yet strangely familiar place, Jay hopes to re-create the magic of those golden childhood summers. And while the spirit of Joe is calling to him, it is actually a similarly haunted, reclusive woman who will ultimately help Jay find himself again.</p>
+            <p className="lead">Welcome to the magical world of Pog Hill and Lansquenet-sous-Tannes!</p>
             <br></br>
             <div className="d-grid gap-2 d-md-flex justify-content-md-center mb-4 mb-lg-3">
               <a href="https://www.ebay.com/sch/i.html?_from=R40&_nkw=blackberry+wine+joanne+harris" target="_blank" rel="noreferrer" className="nav-link px-3 text-dark" >
@@ -24,7 +24,16 @@ function App() {
             <img src="/34229055.jpg" className="img-fluid rounded-3 mb-4" alt="book cover" width={700} height={500} loading="lazy" />
             <AudioPlayer tracks={tracks} />
           </div>
+          <div id="list" className='opaque' style={{display: 'block'}}>
+            {tracks.map((element => {
+              const {title, artist, audioSrc} = element;
+              return (
+                <li key={audioSrc}>"{title}" by <i>{artist}</i></li>
+              )
+            }))}
+          </div>
         </div>
+        
       </div>
   );
 }
--- src/tracks.js
+++ src/tracks.js
@@ -4,6 +4,18 @@ import song3 from './assets/Im Not in Love.mp3';
 import song4 from './assets/18 with a bullet.mp3';
 import song5 from './assets/D-I-V-O-R-C-E.mp3';
 import song6 from './assets/disco queen.mp3';
+// import song7 from './assets/disco queen.mp3';
+// import song8 from './assets/disco queen.mp3';
+import song9 from './assets/The Boys Are Back In Town.mp3';
+import song10 from './assets/Good Vibrations.mp3';
+// import song11 from './assets/disco queen.mp3';
+import song12 from './assets/Hotel California.mp3';
+// import song13 from './assets/disco queen.mp3';
+// import song14 from './assets/disco queen.mp3';
+import song15 from './assets/Kung Fu Fighting.mp3';
+import song16 from './assets/I Feel Love.mp3';
+// import song17 from './assets/disco queen.mp3';
+// import song18 from './assets/disco queen.mp3';
 
 
 export const tracks = [
@@ -47,54 +59,54 @@ export const tracks = [
   //   title: "Sailing",
   //   artist: "Rod Stewart"
   // },
-  // {
-  //   audioSrc: song9,
-  //   title: "Entertainment",
-  //   artist: "Phoenix"
-  // },
-  // {
-  //   audioSrc: song10,
-  //   title: "Entertainment",
-  //   artist: "Phoenix"
-  // },
+  {
+    audioSrc: song9,
+    title: "The Boys Are Back in Town",
+    artist: "Thin Lizzy"
+  },
+  {
+    audioSrc: song10,
+    title: "Good Vibrations",
+    artist: "The Beach Boys"
+  },
   // {
   //   audioSrc: song11,
-  //   title: "Entertainment",
-  //   artist: "Phoenix"
+  //   title: "The Comdine Harvester",
+  //   artist: "The Wurzels"
   // },
-  // {
-  //   audioSrc: song12,
-  //   title: "Entertainment",
-  //   artist: "Phoenix"
-  // },
+  {
+    audioSrc: song12,
+    title: "Hotel California",
+    artist: "Eagles"
+  },
   // {
   //   audioSrc: song13,
-  //   title: "Entertainment",
-  //   artist: "Phoenix"
+  //   title: "The Name Of The Game",
+  //   artist: "ABBA"
   // },
   // {
   //   audioSrc: song14,
-  //   title: "Entertainment",
-  //   artist: "Phoenix"
+  //   title: "Pretty Vacant",
+  //   artist: "Sex Pistols"
   // },
-  // {
-  //   audioSrc: song15,
-  //   title: "Entertainment",
-  //   artist: "Phoenix"
-  // },
-  // {
-  //   audioSrc: song16,
-  //   title: "Entertainment",
-  //   artist: "Phoenix"
-  // },
+  {
+    audioSrc: song15,
+    title: "Kung Fu Fighting",
+    artist: "Carl Douglas"
+  },
+  {
+    audioSrc: song16,
+    title: "I Feel Love",
+    artist: "Donna Summer"
+  },
   // {
   //   audioSrc: song17,
-  //   title: "Entertainment",
-  //   artist: "Phoenix"
+  //   title: "Straighten Out",
+  //   artist: "The Stranglers"
   // },
   // {
   //   audioSrc: song18,
-  //   title: "Entertainment",
-  //   artist: "Phoenix"
+  //   title: "Way Down",
+  //   artist: "Elvis Presley"
   // },
 ];
